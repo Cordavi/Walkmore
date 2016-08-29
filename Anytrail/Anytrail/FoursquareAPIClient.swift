f41d5d2896b78d219a54ca895fa31f6429417b77
Cordavi <michaelamundsen@gmail.com>
2016-08-29 16:28:30 -0400

    Removing unused files

--- Anytrail/Anytrail/FoursquareAPIClient.swift
+++ Anytrail/Anytrail/FoursquareAPIClient.swift
@@ -9,14 +9,6 @@
 import Foundation
 import Alamofire
 import SwiftyJSON
-import CoreLocation
-
-struct FoursquareConstants {
-    static let v = "20160808"
-    static let query = "Historic/Landmarks/Sculptures/Arts/Monuments/Memorial/Landmark/Sculpture/Monument/Art"
-    //"HISTORIC/MONUMENTS/LANDMARKS/STATUES/MUSEUM/POI/HIKING/PARK/FUN/MEMORIAL"
-}
-
 
 class FoursquareAPIClient {
     
@@ -31,34 +23,3 @@ class FoursquareAPIClient {
         }
     }
 }
-
-//
-//        class func getPhotoForVenue(completion: (String?) -> ()) {
-//            let clientID = Keys.fourSquareClientID
-//            let clientSecret = Keys.fourSquareClientSecret
-//
-//            let v = "20160808"
-//            let parameter = ["client_id"     : clientID,
-//                             "client_secret" :clientSecret,
-//                             "v"             : v,
-//                             "VENUE_ID"      : id]
-//
-//
-//
-//
-//            Alamofire.request(.GET, ATConstants.Endpoints.FOURSQUARE_GET_PHOTO, parameters: parameter, headers: nil).responseJSON { (response) in
-//                if let data = response.data {
-//                    let jsonData = JSON(data: data)
-//                    //                for venue in jsonData["response"]["groups"].array! {
-//                    //                    for item in venue["items"] {
-//                    //                        print(item.1.dictionary!["venue"]!["id"])
-//                    //                        print(item.1.dictionary!["venue"]!["name"])
-//                    //                    }
-//                    //                }
-//
-//                    //                completion(jsonData)
-//                }
-//            }
-//        }
-//    }
-//}
